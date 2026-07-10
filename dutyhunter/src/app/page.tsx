16f9b9dd1247f425aeeed645f571a3f84ec8dd4f
Ivan E Sepulveda <ivan.sepulveda2014@gmail.com>
2026-07-09 21:07:27 -0600

    test commit

---
+++ dutyhunter/src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "2rem",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      Hello World! — From Iván
+    </main>
+  );
+}
